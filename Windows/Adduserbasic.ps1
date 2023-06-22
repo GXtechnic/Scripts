167036958a29f9d1f9fad1d0f3ab637a8b573912
Marcelo <130250660+mtc2434@users.noreply.github.com>
2023-06-22 12:01:59 -0700

    basic powershell script for adding users with first and last names

---
+++ Windows/Adduserbasic.ps1
@@ -0,0 +1,18 @@
+﻿# Import AD Module
+Import-Module ActiveDirectory
+
+#Grab variables from user
+$firstname = Read-Host -Prompt "Please enter the first name"
+$lastname = Read-Host -Prompt "Please enter the last name"
+
+# Create AD User
+New-ADUser `
+    -Name "$firstname $lastname" `
+    -GivenName $firstname `
+    -Surname $lastname `
+    -UserPrincipalName "$firstname.$lastname" `
+    -AccountPassword (ConvertTo-SecureString "P@$$w0rd123" -AsPlainText -Force) 
+    #change path name if different on your system`
+    -Path "CN=Users,DC=corp,DC=globex,DC=com" `
+    -ChangePasswordAtLogon 1 `
+    -Enabled 1
